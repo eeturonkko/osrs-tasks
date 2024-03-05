c2d0fbd66311830f167cafc2230d41e51013594b
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-03-05 14:45:44 +0200

    Refactor page component and update metadata

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,14 @@
+<script>
+	import HeroCarousel from '$lib/components/HeroCarousel.svelte';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<section class="flex items-center justify-center gap-8">
+	<div class="flex flex-col items-center gap-5">
+		<h2 class="text-center text-3xl font-bold tracking-tighter md:text-5xl">
+			Your Old School RuneScape tasks, managed.
+		</h2>
+		<Button class="w-1/4" href="/tasks">Get started</Button>
+	</div>
+	<HeroCarousel />
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,12 @@
 <script>
-	import HeroCarousel from '$lib/components/HeroCarousel.svelte';
-	import { Button } from '$lib/components/ui/button';
+	import Hero from '$lib/components/Hero.svelte';
 </script>
 
+<svelte:head>
+	<title>OSRS Tasks | Home</title>
+	<meta name="description" content="Your Old School RuneScape tasks, managed." />
+</svelte:head>
+
 <main class="mt-16 p-8">
-	<section class="flex items-center justify-center gap-8">
-		<div class="flex flex-col items-center gap-5">
-			<h2 class="text-center text-3xl font-bold tracking-tighter md:text-5xl">
-				Your Old School RuneScape tasks, managed.
-			</h2>
-			<Button class="w-1/4" href="/tasks">Get started</Button>
-		</div>
-		<HeroCarousel />
-	</section>
+	<Hero />
 </main>
